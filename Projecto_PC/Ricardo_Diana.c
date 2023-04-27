a2b25f1793a3ae64e9ac003c4a55046f013039c4
Ricardo Marques <105106155+Ricasac@users.noreply.github.com>
2023-04-27 12:23:44 +0100

    Add files via upload

--- Projecto_PC/Ricardo_Diana.c
+++ Projecto_PC/Ricardo_Diana.c
@@ -3,25 +3,27 @@
 
 void menu_inicial()
 {
+    unsigned int option;
     printf("**************************************************************************************************\n");
     printf("*******************************************Hangman Game*******************************************\n");
     printf("**************************************************************************************************\n");
     printf("**************************** Diana Nunes - P4 ** Ricardo Marques - P4 ****************************\n\n\n\n");
+    
     printf("***********************************************Menu***********************************************\n");
-
     printf("1 - Play\n");
     printf("2 - Set Number of retries\n");
     printf("3 - Set difficulty level\n");
     printf("4 - Help\n");
     printf("5 - Exit\n");
+
+    printf("Please input the appropriate number to advance\n");
+    scanf("%u", &option);
+    printf("The number you selected is %u!\n", option);
 }
 
 int main() 
 {
-    unsigned int option;
     menu_inicial();
-    printf("Please input the appropriate number to advance\n");
-    scanf("%u", &option);
-    printf("The number you selected is %u!\n", option);
+    //aplly switch cases
     return 0;
 }
