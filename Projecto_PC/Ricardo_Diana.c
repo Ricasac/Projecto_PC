23ce2edd2b4a99bacc7effcc0653e9de4663632b
Ricardo Marques <105106155+Ricasac@users.noreply.github.com>
2023-05-09 16:55:41 +0100

    Add files via upload

--- Projecto_PC/Ricardo_Diana.c
+++ Projecto_PC/Ricardo_Diana.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void set_dificulty_level()
+{
+    //codezzzzzzzzzzzzzzzzzzz
+}
+
+void help()
+{
+    char menu_return_flag;
+    system("cls");
+    printf("\nGame rules:\n\n"
+    "1st rule: Choose the number of retries you want to start playing (you can choose between 3 and 15 retries)\n\n"
+    "2nd rule: Choose the difficulty level:\n\n"
+    "E to set easy level (for 6 letter words)\nM to define medium level (for 8 letter words)\nH to define difficult level (for 10 letter words)\n\n"
+    "3rd rule: The lines corresponding to the secret word will be displayed and the user must enter letters, when a letter\nbelongs to the word it will appear in the correct place,"
+    "if it does not belong to the word it will be indicated\nthat the letter is not available.\n\n"
+    "4th rule: Good Luck!\n"
+    "**************************************************************************************************\n\n\n");
+    printf("Press x whenever your ready to return to the main menu.\n\n\n");
+    do 
+    {
+        fflush(stdin);
+        scanf("%c", &menu_return_flag);
+        printf("Please input only the 'x' character whenever you are ready to go back to the main menu.\n");
+    } 
+    while (menu_return_flag != 'x' && menu_return_flag != 'X');
+
+    system("cls");
+    return menu_inicial();
+}
+
 void menu_inicial()
 {
     unsigned int option = 6; //inicialize "option" on a number outside of the options
@@ -26,14 +56,33 @@ void menu_inicial()
             printf("Your input is invalid, please try again\n");
         }
     }
-    printf("The number you selected is %u!\n", option);
+
+    switch (option)
+    {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            help();
+            break;
+        case 5:
+            exit(0);
+            break;
+
+        default:
+            break;
+    }
+
 }
-
-
 
 int main() 
 {
     menu_inicial();
-    //aplly switch cases
     return 0;
 }
