40998d89638df301edc1235529a22aa7a2ea92b4
Ricardo Marques <105106155+Ricasac@users.noreply.github.com>
2023-05-10 15:35:22 +0100

    Add files via upload

--- Projecto_PC/Ricardo_Diana.c
+++ Projecto_PC/Ricardo_Diana.c
@@ -1,9 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-void set_dificulty_level()
+#define MAX_WORDS 100
+#define MAX_LEN_WORD 20
+
+void *set_dificulty_level()
 {
-    //codezzzzzzzzzzzzzzzzzzz
+    char file_name[20];
+    char level;
+    char words[MAX_WORDS][MAX_LEN_WORD];
+    int num_words=0;
+
+    printf("Please set your level:\n");
+    printf("E-Easy M-Medium H-Hard\n");
+    printf("In case of incorrect input, it will be set on easy by default!\n");
+    fflush(stdin);
+    scanf(" %c", &level);
+    printf("**************************************************************************************************\n\n");
+
+    switch (level) 
+    {
+        case 'e':
+        case 'E':
+            strcpy(file_name, "easy.txt");
+            break;
+        case 'm':
+        case 'M':
+            strcpy(file_name, "medium.txt");
+            break;
+        case 'h':
+        case 'H':
+            strcpy(file_name, "hard.txt");
+            break;
+        default:
+            strcpy(file_name, "easy.txt");
+            break;
+    }
+
+    FILE *file;
+    file=fopen(file_name, "r");
+
+    if (file == NULL) 
+    {
+        printf("The file wasn't found!\n");
+        return NULL;
+    }
+
+    while (fgets(words[num_words], MAX_LEN_WORD, file)) 
+    {
+        words[num_words][strlen(words[num_words]) - 1] = '\0';
+        num_words++;
+    }
+  
+    srand(time(NULL));
+    char *secret_word = strdup(words[rand() % num_words]);
+
+    fclose(file);
+    system("cls");
+
+    return secret_word;
 }
 
 void help()
@@ -28,57 +85,61 @@ void help()
     while (menu_return_flag != 'x' && menu_return_flag != 'X');
 
     system("cls");
-    return menu_inicial();
+
 }
 
 void menu_inicial()
 {
+    int GameLoop = 1;
     unsigned int option = 6; //inicialize "option" on a number outside of the options
     printf("**************************************************************************************************\n");
     printf("*******************************************Hangman Game*******************************************\n");
     printf("**************************************************************************************************\n");
-    printf("**************************** Diana Nunes - P4 ** Ricardo Marques - P4 ****************************\n\n\n\n");
-    
-    printf("***********************************************Menu***********************************************\n");
-    printf("1 - Play\n");
-    printf("2 - Set Number of retries\n");
-    printf("3 - Set difficulty level\n");
-    printf("4 - Help\n");
-    printf("5 - Exit\n");
+    printf("**************************** Diana Nunes - P4 ** Ricardo Marques - P4 ****************************\n\n");
 
-    printf("Please input the appropriate number to advance\n");
-    while(option < 1 || option > 5)
+    while (GameLoop == 1)
     {
-        fflush(stdin); //clears buffer. Makes sure that if you input something outside of a number, it does not cause havoc
-        scanf("%u", &option);
-        if(option < 1 || option > 5)
+        option=6;
+        printf("***********************************************Menu***********************************************\n");
+        printf("1 - Play\n");
+        printf("2 - Set Number of retries\n");
+        printf("3 - Set difficulty level\n");
+        printf("4 - Help\n");
+        printf("5 - Exit\n");
+
+        printf("Please input the appropriate number to advance\n");
+        while(option < 1 || option > 5)
         {
-            printf("Your input is invalid, please try again\n");
+            fflush(stdin); //clears buffer. Makes sure that if you input something outside of a number, it does not cause havoc
+            scanf("%u", &option);
+            if(option < 1 || option > 5)
+            {
+                printf("Your input is invalid, please try again\n");
+            }
+        }
+
+        switch (option)
+        {
+            case 1:
+                /* code */
+                break;
+            case 2:
+                /* code */
+                break;
+            case 3:
+                set_dificulty_level();
+                break;
+            case 4:
+                help();
+                break;
+            case 5:
+                exit(0);
+                break;
+
+            default:
+                break;
         }
     }
-
-    switch (option)
-    {
-        case 1:
-            /* code */
-            break;
-        case 2:
-            /* code */
-            break;
-        case 3:
-            /* code */
-            break;
-        case 4:
-            help();
-            break;
-        case 5:
-            exit(0);
-            break;
-
-        default:
-            break;
-    }
-
 }
 
 int main() 
